c17fc3a24e8ba14cac94509478b72d9d41181828
Leonardo da Silva Miranda <leomiranda1995@gmail.com>
2022-03-22 22:18:28 -0300

    Definindo tipos para as props
    #Dependência de tipagem de props
    > yarn add prop-types

--- src/App.js
+++ src/App.js
@@ -3,33 +3,34 @@ import React from 'react';
 import Post from './Post';
 import Header from './Header';
 
-const tituloZeroum = 'Titulo Zero Um';
-
 function App() {
   return (
     <>
-      <Header title="JStack's Blog">
+      <Header>
         <h2>Posts da semana</h2>
       </Header>
 
       <hr />
 
       <Post
+        likes={20}
         post={{
           title: 'Titulo da notícia 01',
-          subtitle: 'Subtítulo da notícia 01'
+          subtitle: 'Subtítulo da notícia 01',
         }}
       />
       <Post
+        likes={10}
         post={{
           title: 'Titulo da notícia 02',
-          subtitle: 'Subtítulo da notícia 02'
+          subtitle: 'Subtítulo da notícia 02',
         }}
       />
       <Post
+        likes={50}
         post={{
           title: 'Titulo da notícia 03',
-          subtitle: 'Subtítulo da notícia 03'
+          subtitle: 'Subtítulo da notícia 03',
         }}
       />
     </>
